f78c380c4feb2005266b7537aa63d02412901185
bruce-mig <h180009t@hit.ac.zw>
2023-11-07 15:33:22 +0200

    added functions in main

--- client/main.go
+++ client/main.go
@@ -21,12 +21,12 @@ func main() {
 
 	client := pb.NewGreetServiceClient(conn)
 
-	// names := &pb.NamesList{
-	// 	Names: []string{"Alice", "Bob", "Bruce"},
-	// }
+	names := &pb.NamesList{
+		Names: []string{"Alice", "Bob", "Bruce"},
+	}
 
 	callSayHello(client)
-	// callSayHelloServerStream(client, names)
-	// callSayHelloClientStream(client, names)
-	// callHelloBidirectionalStream(client, names)
+	callSayHelloServerStream(client, names)
+	callSayHelloClientStream(client, names)
+	callHelloBidirectionalStream(client, names)
 }
